a7ffadd0e41cd323e13f83b3ac570abda438bf4c
Caro <55109108+CaroMen@users.noreply.github.com>
2021-05-11 09:47:01 -0500

    header responsive css done

--- src/components/IntroSection/Header.elements.js
+++ src/components/IntroSection/Header.elements.js
@@ -5,6 +5,10 @@ display: flex;
 margin: 50px -15px -15px -15px;
 flex-wrap: wrap;
 align-items: center;
+
+@media screen and (max-width: 768px) {
+    /* margin-top: -100px; */
+}
 `
 
 export const HeaderColumn = styled.div`
@@ -14,12 +18,24 @@ padding-left: 15px;
 flex: 1;
 max-width: 50%;
 flex-basis: 50%;
+
+@media screen and (max-width: 768px) {
+    max-width: 100%;
+    flex-basis: 100%;
+    display: flex;
+    justify-content: center;
+}
 `
 
 export const TextWrapper = styled.div`
 max-width: 540px;
 padding-top: 0px;
 padding-bottom: 60px;
+
+@media screen and (max-width: 768px) {
+    padding-bottom: 125px;
+    /* padding-top: 100px; */
+}
 `
 export const TopLine = styled.div`
 color: #2E3630;
@@ -55,6 +71,11 @@ box-shadow:
     0 6px 5px rgba(0, 0, 0, 0.048),
     0 12px 10px rgba(0, 0, 0, 0.06),
     0 22px 17px rgba(0, 0, 0, 0.052);
+
+@media screen and (max-width: 768px) {
+    margin-top: 105px;
+    margin-bottom: 50px;
+}
 `
 
 export const SocialIconLink = styled.a`

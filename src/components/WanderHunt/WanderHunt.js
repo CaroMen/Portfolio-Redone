e53888542456d8a30b582a0448d949b231bdebfa
Caro <55109108+CaroMen@users.noreply.github.com>
2021-05-14 09:16:23 -0500

    wanderhunt page started

---
+++ src/components/WanderHunt/WanderHunt.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from '../../globalStyles';
+import { } from './Projects.elements';
+
+const WanderHunt = () => {
+    return (
+        <>
+            <Container>
+
+            </Container>
+        </>
+    )
+}
+
+export default WanderHunt;

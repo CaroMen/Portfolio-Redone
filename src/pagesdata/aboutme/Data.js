4e907b2c61c77b2fcfb9f16e0b1c32c71572c15f
Caro <55109108+CaroMen@users.noreply.github.com>
2021-05-14 20:46:44 -0500

    data fixed

--- src/pagesdata/aboutme/Data.js
+++ src/pagesdata/aboutme/Data.js
@@ -1,6 +1,6 @@
 export const aboutMeInfo = {
     headline: 'About Me',
-    pOne: "I'm a mostly self taught developer and made the decision to focus on software development in 2019. During my last two years at Baylor, I spent a lot of time working with Python and found out I enjoy working on technically complex and challenging problems. Since 2019, I've spent my free time studying Python, JavaScript, working with React and creating full-stack applications. I decided to pursure further education in programing by attending App Academy where I spent 6 months in an intensive 6 month program focusing on full-stack applications. Programming has become an outlet for me, so I'm passionate about teaching others about what amazing things programming can do for you.",
+    pOne: "I'm a mostly self taught developer and made the decision to focus on software development in 2019. During my last two years at Baylor, I spent a lot of time working with Python and found out I enjoy working on technically complex and challenging problems. Since 2019, I've spent my free time studying Python, JavaScript, working with React and creating full-stack applications. I decided to pursure further education in programing by attending App Academy where I took part in an intensive 6 month program focusing on full-stack applications. Programming has become an outlet for me, so I'm passionate about teaching others about what amazing things programming can do for you.",
     skillOne: 'Python',
     skillTwo: 'JavaScript',
     skillThree: 'HTML',

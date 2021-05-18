11ec5d89986425c3edab2fe04c271b0614587814
Caro <55109108+CaroMen@users.noreply.github.com>
2021-05-18 17:16:37 -0500

    link added to logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
         <>
             <Nav>
                 <NavbarContainer>
-                    <Logo>
+                    <Logo to="/">
                         <NavIcon />
                         CARO.
                     </Logo>

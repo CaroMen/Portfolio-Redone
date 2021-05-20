3d513678b393a96c64ceed9410ad18b1e2c87a00
Caro <55109108+CaroMen@users.noreply.github.com>
2021-05-19 19:38:58 -0500

    css fixed

--- src/components/SpireCamp/SpireCamp.elements.js
+++ src/components/SpireCamp/SpireCamp.elements.js
@@ -177,7 +177,8 @@ box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
 
 @media screen and (max-width: 812px) {
     width: 100%;
-    height: 150px;
+    height: 100%;
+    /* height: 150px; */
     margin-top: -50px;
 }
 `

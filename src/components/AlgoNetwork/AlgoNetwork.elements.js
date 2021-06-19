e0afe3fd5c6dcbeb7ff44f2c094fed043c17a289
Caro <caroline_mendez@alumni.baylor.edu>
2021-06-18 21:18:40 -0500

    new project page started

---
+++ src/components/AlgoNetwork/AlgoNetwork.elements.js
@@ -0,0 +1,310 @@
+import styled from 'styled-components';
+
+export const WHTitle = styled.div`
+font-size: 30px;
+font-family: 'Montserrat', sans-serif;
+margin-top: 40px;
+`;
+
+export const WHDesc = styled.div`
+font-family: 'Lato', sans-serif;
+font-size: 17px;
+margin-top: 20px;
+line-height: 2em;
+width: 68%;
+
+@media screen and (max-width: 812px) {
+    width: 100%;
+}
+`;
+
+export const WHTechInfo = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+margin-top: 80px;
+
+@media screen and (max-width: 812px) {
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+}
+`;
+
+export const WHType = styled.div`
+font-family: 'Lato', sans-serif;
+margin-right: 50px;
+
+@media screen and (max-width: 812px) {
+    width: 100%;
+    text-align: center;
+    margin-bottom: 20px;
+}
+`
+
+export const WHStack = styled.div`
+margin-right: 50px;
+
+@media screen and (max-width: 812px) {
+    width: 100%;
+    text-align: center;
+    margin-bottom: 20px;
+}
+`
+
+export const WHSOne = styled.div`
+font-family: 'Lato', sans-serif;
+padding-bottom: 5px;
+`
+
+export const WHSTwo = styled.div`
+font-family: 'Lato', sans-serif;
+padding-bottom: 5px;
+`
+
+export const WHSThree = styled.div`
+font-family: 'Lato', sans-serif;
+padding-bottom: 5px;
+`
+
+export const WHSFour = styled.div`
+font-family: 'Lato', sans-serif;
+padding-bottom: 5px;
+`
+export const WHSFive = styled.div`
+font-family: 'Lato', sans-serif;
+`
+
+export const TypeTitle = styled.div`
+font-family: 'Montserrat', sans-serif;
+margin-bottom: 10px;
+
+@media screen and (max-width: 812px) {
+    font-size: 18px;
+}
+`;
+
+export const StackTitle = styled.div`
+font-family: 'Montserrat', sans-serif;
+margin-bottom: 10px;
+
+@media screen and (max-width: 812px) {
+    font-size: 18px;
+}
+`;
+
+export const WHLiveLink = styled.div`
+margin-right: 50px;
+
+@media screen and (max-width: 812px) {
+    width: 100%;
+    text-align: center;
+    margin-bottom: 20px;
+}
+`
+
+export const LinkTitle = styled.div`
+margin-bottom: 10px;
+
+@media screen and (max-width: 812px) {
+    font-size: 18px;
+}
+`
+
+export const SiteLink = styled.a`
+text-decoration: none;
+color: inherit;
+font-family: 'Lato', sans-serif;
+color: #73588E;
+font-weight: 800;
+
+&:hover{
+    font-size: 17px;
+}
+`
+export const WHCode = styled.div`
+@media screen and (max-width: 812px) {
+    width: 100%;
+    text-align: center;
+    margin-bottom: 20px;
+}
+`
+
+export const CodeTitle = styled.div`
+margin-bottom: 10px;
+
+@media screen and (max-width: 812px) {
+    font-size: 18px;
+}
+`
+
+export const CodeLink = styled.a`
+text-decoration: none;
+color: inherit;
+font-family: 'Lato', sans-serif;
+color: #73588E;
+font-weight: 800;
+
+&:hover{
+    font-size: 17px;
+}
+`
+export const HomePageWH = styled.div`
+margin-top: 80px;
+text-align: center;
+`
+
+export const WHPic = styled.img`
+border-radius: 10px;
+width: 80%;
+height: 60%;
+box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
+
+@media screen and (max-width: 812px) {
+    width: 100%;
+    height: 150px;
+    margin-top: -50px;
+}
+`
+export const WHPurpose = styled.div`
+margin-top: 80px;
+`
+
+export const PurposeTitle = styled.div`
+font-size: 30px;
+font-family: 'Montserrat', sans-serif;
+`
+
+export const PurposeParagraph = styled.div`
+font-family: 'Lato', sans-serif;
+font-size: 17px;
+margin-top: 20px;
+line-height: 2em;
+width: 68%;
+
+@media screen and (max-width: 812px) {
+    width: 100%;
+    margin-bottom: 20px;
+}
+`
+export const WHCodeStack = styled.div`
+margin-top: 50px;
+display: flex;
+flex-direction: row;
+justify-content: flex-end;
+margin-bottom: 80px;
+
+@media screen and (max-width: 812px) {
+    display: flex;
+    flex-direction: column-reverse;
+}
+`
+
+export const StackCodeTitle = styled.div`
+font-size: 30px;
+font-family: 'Montserrat', sans-serif;
+text-align: right;
+`
+
+export const StackParagraph = styled.div`
+font-family: 'Lato', sans-serif;
+font-size: 17px;
+margin-top: 20px;
+line-height: 2em;
+width: 100%;
+text-align: right;
+`
+
+export const StackDiv = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: flex-end;
+
+`
+
+export const StackWrapper = styled.div`
+display: flex;
+flex-wrap: wrap;
+flex-direction: row;
+width: 100%;
+height: 100px;
+margin-top: 55px;
+justify-content: center;
+
+@media screen and (max-width: 812px) {
+    margin-top: 20px;
+}
+`
+
+export const Stack = styled.div`
+font-size: 14px;
+padding: 8px 15px 8px 15px;
+background-color: #f1f1f1;
+margin-right: 10px;
+border-radius: 5px;
+box-shadow:
+    0 2px 2px rgba(0, 0, 0, 0.034),
+    0 6px 5px rgba(0, 0, 0, 0.048);
+margin-top: 15px;
+font-family: 'Open Sans', sans-serif;
+
+&:hover {
+    background-color: #e9e9e9;
+}
+`
+export const WHLessons = styled.div`
+margin-top: 80px;
+margin-bottom: 80px;
+
+`
+
+export const LessonsTitle = styled.div`
+font-size: 30px;
+font-family: 'Montserrat', sans-serif;
+`
+
+export const LessonsParagraph = styled.div`
+font-family: 'Lato', sans-serif;
+font-size: 17px;
+margin-top: 20px;
+line-height: 2em;
+width: 75%;
+
+@media screen and (max-width: 812px) {
+    width: 100%;
+}
+`
+
+export const WHPics = styled.div`
+display: flex;
+flex-direction: row;
+margin-top: 80px;
+
+@media screen and (max-width: 812px) {
+    display: flex;
+    flex-direction: column;
+}
+`
+
+export const WHPicOne = styled.img`
+width: 50%;
+margin-right: 30px;
+border-radius: 10px;
+box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
+
+
+@media screen and (max-width: 812px) {
+    width: 100%;
+    margin-bottom: 30px;
+}
+`;
+
+export const WHPicTwo = styled.img`
+width: 50%;
+border-radius: 10px;
+box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
+
+@media screen and (max-width: 812px) {
+    width: 100%;
+}
+`
---
+++ src/components/AlgoNetwork/AlgoNetwork.js
@@ -0,0 +1,84 @@
+import React, { useEffect } from 'react';
+import { Container } from '../../globalStyles';
+import HomePic from '../../images/wh-home.png'
+import PicOne from '../../images/WH-1.png'
+import PicTwo from '../../images/WH-2.png'
+import { WHTitle, WHDesc, WHType, WHStack, WHSOne, WHSTwo, WHSThree, WHSFour, WHSFive, WHTechInfo, TypeTitle, StackTitle, WHLiveLink, LinkTitle, SiteLink, WHCode, CodeTitle, CodeLink, HomePageWH, WHPic, WHPurpose, PurposeTitle, PurposeParagraph, WHCodeStack, StackCodeTitle, StackParagraph, StackDiv, StackWrapper, Stack, WHLessons, LessonsTitle, LessonsParagraph, WHPics, WHPicOne, WHPicTwo } from './WanderHunt.elements';
+
+const WanderHunt = ({ title1, desc1, type1, stack1, stack2, stack3, stack4, stack5, purpose1, stackchosen1, lessons1 }) => {
+
+    useEffect(() => {
+        window.scroll(0, 0)
+    })
+
+    return (
+        <>
+            <Container>
+                <WHTitle>{title1}</WHTitle>
+                <WHDesc>{desc1}</WHDesc>
+                <WHTechInfo>
+                    <WHType>
+                        <TypeTitle>Type</TypeTitle>
+                        {type1}
+                    </WHType>
+                    <WHStack>
+                        <StackTitle>Stack</StackTitle>
+                        <WHSOne>
+                            {stack1}
+                        </WHSOne>
+                        <WHSTwo>
+                            {stack2}
+                        </WHSTwo>
+                        <WHSThree>
+                            {stack3}
+                        </WHSThree>
+                        <WHSFour>
+                            {stack4}
+                        </WHSFour>
+                        <WHSFive>
+                            {stack5}
+                        </WHSFive>
+                    </WHStack>
+                    <WHLiveLink>
+                        <LinkTitle>Take Me To The Site</LinkTitle>
+                        <SiteLink href={"https://wanderhunt.herokuapp.com/"}>Live Site</SiteLink>
+                    </WHLiveLink>
+                    <WHCode>
+                        <CodeTitle>Show Me The Code</CodeTitle>
+                        <CodeLink href={"https://github.com/CaroMen/WanderHunt"}>Repo</CodeLink>
+                    </WHCode>
+                </WHTechInfo>
+                <HomePageWH>
+                    <WHPic src={HomePic} />
+                </HomePageWH>
+                <WHPurpose>
+                    <PurposeTitle>Project Purpose</PurposeTitle>
+                    <PurposeParagraph>{purpose1}</PurposeParagraph>
+                </WHPurpose>
+                <WHCodeStack>
+                    <StackWrapper>
+                        <Stack>{stack1}</Stack>
+                        <Stack>{stack2}</Stack>
+                        <Stack>{stack3}</Stack>
+                        <Stack>{stack4}</Stack>
+                        <Stack>{stack5}</Stack>
+                    </StackWrapper>
+                    <StackDiv>
+                        <StackCodeTitle>Project Stack</StackCodeTitle>
+                        <StackParagraph>{stackchosen1}</StackParagraph>
+                    </StackDiv>
+                </WHCodeStack>
+                <WHPics>
+                    <WHPicOne src={PicOne} />
+                    <WHPicTwo src={PicTwo} />
+                </WHPics>
+                <WHLessons>
+                    <LessonsTitle>Lessons Learned</LessonsTitle>
+                    <LessonsParagraph>{lessons1}</LessonsParagraph>
+                </WHLessons>
+            </Container>
+        </>
+    )
+}
+
+export default WanderHunt;
